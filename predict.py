7d4a0c5c539abcf670c876fcc1040d2268421e04
ihsgnef <shifeng@cs.umd.edu>
2018-10-11 11:38:06 -0400

    predict running on codalab

--- predict.py
+++ predict.py
@@ -5,9 +5,6 @@ import signal
 import requests
 import subprocess
 from qanta.tfidf import web
-import logging
-
-logging.getLogger("requests").setLevel(logging.WARNING)
 
 
 def _web(input_dir):
@@ -20,35 +17,31 @@ def _web(input_dir):
 @click.argument('output_dir')
 @click.option('--char_step_size', default=25)
 def predict(code_dir, input_dir, output_dir, char_step_size):
-    '''start web service'''
     web_proc = subprocess.Popen(['python', '-m', code_dir, 'web'],
                                 preexec_fn=os.setsid,
-                                stdout=subprocess.PIPE
-                                )
+                                stdout=subprocess.PIPE)
     output = ''
     while 'Debug mode' not in output:
         output = web_proc.stdout.readline().decode('utf-8')
-        # print('#', output)
 
-    '''get predictions'''
     url = 'http://0.0.0.0:4861/api/1.0/quizbowl/act'
-    results = {}
+    results = []
     questions = json.load(open(input_dir))['questions']
-    for q in questions[:10]:
-        answers = {}
+    for question_idx, q in enumerate(questions[:10]):
+        results.append([])
+        sent_tokenizations = q['tokenizations']
         # get an answer every K characters
-        for char_index in range(0, len(q['text']), char_step_size):
-            # this query defines the input to the models
-            query = {
-                'text': q['text'][:char_index],
-                'qanta_id': q['qanta_id'],
-                'char_index': char_index,
-                'sent_index': 0,  # TODO
-                # TODO extra stuff including game state
-            }
-            resp = requests.post(url, data=query)
-            answers[char_index] = json.loads(resp.content.decode('utf-8'))
-        results[q['qanta_id']] = answers
+        for sent_idx, (sent_st, sent_ed) in enumerate(sent_tokenizations):
+            for char_idx in range(sent_st, sent_ed, char_step_size):
+                query = {
+                    'question_idx': question_idx,
+                    'sent_index': sent_idx,
+                    'char_index': char_idx,
+                    'text': q['text'][:char_idx]
+                }
+                resp = requests.post(url, data=query).content.decode('utf-8')
+                query.update(json.loads(resp))
+                results[-1].append(query)
 
     with open(output_dir, 'w') as f:
         json.dump(results, f)
